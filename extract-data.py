1ef6b9dba7e4b13479df791a21f60c21b15ee1da
Frank Hofmann <frank.hofmann@efho.de>
2023-12-09 15:57:23 +0100

    add table with individual weights per tld

--- extract-data.py
+++ extract-data.py
@@ -14,7 +14,19 @@ import pandas as pd
 datafile = "majestic_million.csv"
 
 # - default number of datasets for output
-numberOfDatasets = 20
+numberOfDatasets = 100
+
+# - define individual weights per tld as a data frame
+#   * define columns for weight and tlds
+weights = [1.0, 0.5, 0.2]
+tlds = ["default", "com", "tk"]
+#   * transform these into a Pandas Series
+s1 = pd.Series(tlds)
+s2 = pd.Series(weights)
+#   * define column description with tld as 1st column, and weight as 2nd column
+d = {'tld': s1, 'weight': s2}
+#   * create a data frame from it
+individualWeights = pd.DataFrame(d)
 
 # read data file as comma-separated CSV
 data = pd.read_csv(datafile, sep=",")
@@ -27,6 +39,6 @@ for tld in listOfTlds:
     # select the corresponding subset per tld
     subData = data[data["TLD"] == tld]
 
-    print(subData.head(numberOfDatasets))
+#    print(subData.head(numberOfDatasets))
 
-    break
+#    break
